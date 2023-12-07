86a348300f95b30fffa898d7cb34d737fd8a4e5c
Khodosov Eugene <nnagatoo@mail.ru>
2023-12-08 02:25:37 +0300

    prefix

--- core/src/ru/numbdev/dungen/screen/MainGameScreen.java
+++ core/src/ru/numbdev/dungen/screen/MainGameScreen.java
@@ -2,8 +2,18 @@ package ru.numbdev.dungen.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,39 +35,94 @@ public class MainGameScreen implements DungeonScreen {
     private final List<Sprite> controlElements = new ArrayList<>();
     private Player player;
 
-    // Переделать на https://gist.github.com/Leejjon/7fb8aa3ea2e4024a9eba31fa4f3339fb
+    private Stage stage;
+    private Skin skin;
+
     public MainGameScreen(Dungen game) {
         this.game = game;
-        player = new Player();
-        inputEngine = new GameInputEngine(player);
-
-        Player test = new Player();
-        test.setPosition(300, 300);
-        actorElements.add(test);
-        actorElements.add(player);
+//        player = new Player();
+//        inputEngine = new GameInputEngine(player);
+//
+//        Player test = new Player();
+//        test.setPosition(300, 300);
+//        actorElements.add(test);
+//        actorElements.add(player);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0.2f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        game.batch.begin();
+        stage.act();
+        stage.draw();
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//        game.batch.begin();
 
-        for(Actor actor : actorElements) {
-            actor.draw(game.batch, 0);
-        }
-        for (Sprite element : gameplayElements) {
-            element.draw(game.batch);
-        }
-        for (Sprite element : controlElements) {
-            element.draw(game.batch);
-        }
-        game.batch.end();
+//        for(Actor actor : actorElements) {
+//            actor.draw(game.batch, 0);
+//        }
+//        for (Sprite element : gameplayElements) {
+//            element.draw(game.batch);
+//        }
+//        for (Sprite element : controlElements) {
+//            element.draw(game.batch);
+//        }
+//        game.batch.end();
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(inputEngine);
+        stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
+
+        Gdx.input.setInputProcessor(stage);
+        skin = new Skin(Gdx.files.internal("data/skins/skin.json"));
+
+        buildMenu();
+    }
+
+    private void buildMenu(){
+        Table menuTable = new Table(skin);
+        menuTable.setPosition(0, 0);
+        menuTable.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        menuTable.align(Align.top);
+
+        stage.addActor(menuTable);
+
+        Texture texture = new Texture(Gdx.files.internal("data/ubuntulogo.png"));
+        Image image = new Image(texture);
+
+        //first option
+        image.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //your action on click
+            }
+        });
+        menuTable.add(image);
+
+        //second option
+        ImageButton imageButton = new ImageButton(image.getDrawable());
+        imageButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //your action on click
+            }
+        });
+        menuTable.add(imageButton);
+
+        //third option
+        Button button = new Button(skin);
+        button.add(image);
+        button.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //your action on click
+            }
+        });
+        menuTable.add(button);
+
     }
 
     @Override
@@ -77,7 +142,8 @@ public class MainGameScreen implements DungeonScreen {
 
     @Override
     public void hide() {
-
+        stage = null;
+        skin = null;
     }
 
     @Override
